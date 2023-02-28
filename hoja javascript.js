a7a8e3737622c75bf654298bc160a299ce4b3fbd
Juan Manuel Muñoz Tuttolomondo <juanmanuelmunoztutto@gmail.com>
2023-02-28 10:07:36 -0300

    Guardado del versionado de todos los archivos del trabajo final

---
+++ hoja javascript.js
@@ -0,0 +1,10 @@
+
+/** agregamos un evento para conocer los datos de contacto en el header */
+
+document.getElementById('boton').onclick = function () {
+    console.log("captura de evento clic")
+    document.getElementById("contactar").innerHTML = "JP@gmail.com - Celular +54115894658"
+}
+
+
+
